606b833b80de912aab5183387dbb4e6916e585b7
silviabulacu <114721556+silviabulacu@users.noreply.github.com>
2022-10-19 20:04:17 +0300

    Homework01b - Arithmetic calculator

--- src/main/Homework/com/siit/homework/homework01.java
+++ src/main/Homework/com/siit/homework/homework01.java
@@ -4,6 +4,8 @@ package com.siit.homework;
 
 public class homework01 {
 
+    // Display the number of days in month February from a year between 1900-2016 that is read from keyboard
+
 public static void main(String[] args) {
     int year = Integer.parseInt(args[0]);
      int x = 1900;
@@ -25,10 +27,6 @@ public static void main(String[] args) {
      }
 }
 
-
-
-// Display the number of days in month February from a year between 1900-2016 that is read from keyboard
-
     public static boolean isLeapYear(int year) {
         if (year % 4 != 0) {
             return false;
@@ -46,17 +44,4 @@ public static void main(String[] args) {
 
     //Implement an arithmetic calculator that takes 3 arguments: the first number, the operation, the second number.
 
-//    public static void main(String[] args)
-//    {
-//        if (args.length == 3)
-//        {
-//            float a = Float.parseFloat(args[0]);
-//            float b = Float.parseFloat(args[2]);0
-//            String operator = args[1];
-// implement calculator logic here
-//            System.out.println("Result is: " + result);
-//        }
-//        else {
-//            System.err.println("3 args are needed!");
-//        }
-//    }
+
